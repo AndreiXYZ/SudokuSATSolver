4ec418c52ef9d935b701915946cdacd12a833c2a
Apostol Andrei <apo.andrei23@yahoo.com>
2019-02-23 00:27:10 +0100

    added stats generation for all heuristic+metric combinations

--- generate_stats.py
+++ generate_stats.py
@@ -1,14 +1,44 @@
 import pickle
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
+import numpy as np
+
+def removeOutliers(runtimes):
+	xvals, yvals = zip(*runtimes)
+	dataMean = np.mean(yvals)
+	dataStd = np.std(yvals)
+	filtered = [(x,y) for x,y in runtimes if (dataMean - 2*dataStd < y < dataMean + 2*dataStd)]
+	print(f'Removed {len(xvals)-len(filtered)} outliers')
+	return filtered
 
 
-with open('backtrack_random.pkl', 'rb') as f:
-	runtimes = pickle.load(f)
+def getStats(filename):
+	with open(filename, 'rb') as f:
+		measurements = pickle.load(f)
 
-xvals, yvals = zip(*runtimes)
-plt.plot(xvals, yvals, 'ro')
-plt.show()
+	#Remove outliers for plotting only
+	xvals, yvals = zip(*removeOutliers(measurements))
+	plt.xticks(range(21,32))
+	plt.yticks(range(0,int(max(xvals))))
+	plt.grid()
+	plt.title(heuristic + ' ' + metric)
+	plt.xlabel('Num. givens')
+	plt.ylabel(metric)
+	plt.plot(xvals, yvals, 'bo')
+	plt.show()
 
-corr, pval = pearsonr(xvals, yvals)
-print(f'corr= {corr}, pval= {pval}')
+	#Run Pearson on data without outliers
+	corr, pval = pearsonr(xvals, yvals)
+	print(f'No outliers: corr= {corr}, pval= {pval}')
+	#Run Pearson test on original data
+	xvals, yvals = zip(*measurements)
+	corr, pval = pearsonr(xvals, yvals)
+	print(f'With outliers: corr= {corr}, pval= {pval}')
+	print('-'*50)
+
+metrics = ['backtrack', 'runtimes']
+heuristics = ['randoms', 'jeroslow', 'diag', 'dlis']
+
+for metric in metrics:
+	for heuristic in heuristics:
+		getStats(metric + '_' + heuristic + '.pkl')
