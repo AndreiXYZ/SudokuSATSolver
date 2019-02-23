559f135959846d246bdbeaba9d96f0c960faa31c
Apostol Andrei <apo.andrei23@yahoo.com>
2019-02-23 03:12:33 +0100

    added heatmaps

--- generate_stats.py
+++ generate_stats.py
@@ -1,7 +1,9 @@
 import pickle
 import matplotlib.pyplot as plt
 from scipy.stats.stats import pearsonr
+from scipy.stats import spearmanr, kendalltau, mannwhitneyu
 import numpy as np
+import seaborn as sns
 
 def removeOutliers(runtimes):
 	xvals, yvals = zip(*runtimes)
@@ -11,13 +13,43 @@ def removeOutliers(runtimes):
 	print(f'Removed {len(xvals)-len(filtered)} outliers')
 	return filtered
 
+def createHeatmap(measurements):
+	xvals, yvals = zip(*measurements)
+	xvals = np.array(xvals)
+	yvals = np.array(yvals)
 
-def getStats(filename):
+	ydim = yvals.max()+1
+	xdim = xvals.max()-20
+	
+	twodvals = np.concatenate((xvals[:,np.newaxis],yvals[:,np.newaxis]), axis=1)
+	unique, counts = np.unique(twodvals.astype(np.int8), return_counts=True, axis=0)
+	countsArray = np.concatenate((unique,counts[:,np.newaxis]), axis=1)
+
+	a = np.zeros((int(yvals.max()+1),int(xvals.max()-20)))
+	for x,y,z in countsArray:
+		a[int(y)][int(x-21)] = z
+
+	ax = sns.heatmap(a, annot=True, xticklabels=np.arange(21,32), yticklabels=np.arange(0,ydim-1).astype(np.int8),
+					 linewidth=0.5)
+	plt.xlabel('num. givens')
+	plt.ylabel('num. backtracks')
+	ax.invert_yaxis()
+	plt.title(heuristic + ' ' + metric + ' heatmap')
+	plt.show()
+
+def getStats(heuristic, metric):
+	filename = metric + '_' + heuristic + '.pkl'
+	print('Stats for ', filename)
 	with open(filename, 'rb') as f:
 		measurements = pickle.load(f)
 
 	#Remove outliers for plotting only
-	xvals, yvals = zip(*removeOutliers(measurements))
+	measurementsCurated = removeOutliers(measurements)
+	
+	#Create heatmap
+	createHeatmap(measurementsCurated)
+	
+	xvals, yvals = zip(*measurementsCurated)
 	plt.xticks(range(21,32))
 	plt.yticks(range(0,int(max(xvals))))
 	plt.grid()
@@ -26,9 +58,10 @@ def getStats(filename):
 	plt.ylabel(metric)
 	plt.plot(xvals, yvals, 'bo')
 	plt.show()
-
 	#Run Pearson on data without outliers
-	corr, pval = pearsonr(xvals, yvals)
+	# corr, pval = pearsonr(xvals, yvals)
+	corr, pval = spearmanr(xvals, yvals)
+	# corr, pval = kendalltau(xvals, yvals)
 	print(f'No outliers: corr= {corr}, pval= {pval}')
 	#Run Pearson test on original data
 	xvals, yvals = zip(*measurements)
@@ -41,4 +74,6 @@ heuristics = ['randoms', 'jeroslow', 'diag', 'dlis']
 
 for metric in metrics:
 	for heuristic in heuristics:
-		getStats(metric + '_' + heuristic + '.pkl')
+		getStats(heuristic, metric)
+# getStats('jeroslow', 'backtrack')
+# getStats('jeroslow', 'runtimes')
